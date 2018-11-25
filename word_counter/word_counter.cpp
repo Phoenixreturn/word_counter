3a420e828fa4304698ee7a5a816e047ba6766953
Phoenix <rob.gallyamov@gmail.com>
2018-11-25 15:00:40 +0500

    test

---
+++ word_counter/word_counter.cpp
@@ -0,0 +1,12 @@
+﻿// word_counter.cpp: определяет точку входа для приложения.
+//
+
+#include "word_counter.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
